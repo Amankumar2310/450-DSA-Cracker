430bda999bbb07f8b335e193a7917ae946f511b9
Aman kumar <60618495+Amankumar2310@users.noreply.github.com>
2022-02-02 21:20:29 +0530

    Update reverseString.java

--- String/reverseString.java
+++ String/reverseString.java
@@ -24,3 +24,42 @@ class Reverse
     }
     
 }
+
+
+/******************************************************** Other Method***********************************************/
+// import java.util.*;
+// import java.io.*;
+// import java.lang.*;
+// class Reverse
+// {
+//     public static void main(String args[]){
+//         Scanner sc=new Scanner(System.in);
+//         int t=sc.nextInt();
+//         sc.nextLine();
+//         while(t-- >0)
+//         {
+//             String str= sc.nextLine();
+//             System.out.println(reverse(str));
+//         }
+//     }
+//     static String reverse(String str)
+//     {
+//         int n=str.length();
+//         if(n==0)
+//            return "";
+        
+//         int l=0, r=n-1;
+//         char[] ch=str.toCharArray();
+//         while(l<r)
+//         {
+//             char c=ch[l];
+//             ch[l]=ch[r];
+//             ch[r]=c;
+//             l++;
+//             r--;
+//         }
+//         return new String(ch);
+            
+//     }
+    
+// }
